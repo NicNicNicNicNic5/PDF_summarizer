6c5035de224a52d706b58c27c9768cc89cbfd126
NicNicNicNicNic5 <suardimannicholas@gmail.com>
2024-12-06 19:38:55 +0700

    fix faiss

--- PDF_summarizer.py
+++ PDF_summarizer.py
@@ -83,11 +83,8 @@ if done_uploading == True:
             # Load API KEY from .env
             load_dotenv()
 
-            # Access API KEY
-            # groq_api_key = os.getenv("GROQ_API_KEY")
-
             # Use model with Groq
-            llm = ChatGroq(model_name="llama3-8b-8192", temperature=0, groq_api_key=os.environ[os.getenv("GROQ_API_KEY")])
+            llm = ChatGroq(model_name="llama3-8b-8192", temperature=0, groq_api_key=os.environ[os.environ("GROQ_API_KEY")])
 
             # Define the prompt template for generating AI responses
             PROMPT_TEMPLATE = """
